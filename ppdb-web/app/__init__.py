2b767a3ca5b541d1216e5e1db8633ac2dc37446b
irene krismawati <irene@example.com>
2025-05-08 13:12:11 +0700

    Refactor login page and update database URI; add username field to User model

--- ppdb-web/app/__init__.py
+++ ppdb-web/app/__init__.py
@@ -2,19 +2,15 @@ import os
 from flask import Config, Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_migrate import Migrate 
 
-migrate = Migrate()
 
 # Inisialisasi ekstensi
 db = SQLAlchemy()
-
 login_manager = LoginManager()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    # Konfigurasi aplikasi
-    migrate.init_app(app, db)   
+    # Konfigurasi aplikasi  
     # Add upload folder configuration
     app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static/profile_pics')
     app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024  # 1MB max
@@ -23,7 +19,7 @@ def create_app(config_class=Config):
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     
     app.config['SECRET_KEY'] = 'mysecret'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todolist.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ppdb.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Inisialisasi ekstensi
